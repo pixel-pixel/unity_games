bcefea605982625564317ee3ce8e217856f10f15
Andrii Bartish <58302316+pixel-pixel@users.noreply.github.com>
2022-02-04 10:53:17 +0200

    Imposter's dead (#2)
    * Add animations to Imposter
    
    * Add blood particles
    
    * Bullet phisics

---
+++ bullet_game/Assets/Scripts/Imposter.cs
@@ -0,0 +1,23 @@
+using System;
+using UnityEngine;
+
+public class Imposter : MonoBehaviour
+{
+    public GameObject blood;
+    private Animator _animator;
+    
+    void Start()
+    {
+        _animator = GetComponent<Animator>();
+    }
+    
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Bullet" && !_animator.GetBool("isDead"))
+        {
+            _animator.SetBool("isDead", true);
+            Instantiate(blood, transform.position, Quaternion.identity);
+            
+        }
+    }
+}
